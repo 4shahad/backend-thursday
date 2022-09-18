aba131926acad20bb6af429ab4fc5aa06b39e3e1
Shahad Zam <shahaaz66@gmail.com>
2022-09-18 10:23:42 +0300

    sundaycommit

--- src/main/java/com/example/rentaperson/config/SecurityConfig.java
+++ src/main/java/com/example/rentaperson/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .cors().configurationSource(corsConfigurationSource())
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/v1/user/register","/api/v1/user/viewAllPersons","/api/v1/user/viewByCategory/{cat}","/api/v1/user/viewByCity/{city}","/api/v1/review/reviewByPerson/{username}","/api/v1/review/aveOfPerson/{username}","viewByCategoryWithRate/{cat}").permitAll()
+                .antMatchers("/api/v1/user/register","/api/v1/user/viewAllPersons","/api/v1/user/viewByCategory/{cat}","/api/v1/user/viewByCity/{city}","/api/v1/review/reviewByPerson/{username}","/api/v1/review/aveOfPerson/{username}","/api/v1/user/viewByCategoryWithRate/{cat}").permitAll()
                 .antMatchers("/api/v1/user/viewAll").hasAuthority("ADMIN")
                 .antMatchers("/api/v1/skill/viewAllSkills").hasAuthority("ADMIN")
                 .antMatchers("/api/v1/appointment/viewAll").hasAuthority("ADMIN")
